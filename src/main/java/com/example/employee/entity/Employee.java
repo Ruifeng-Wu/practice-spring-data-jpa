82af46b7c733799fb9bdb023a8f51fdb26f80919
Ruifeng-Wu <296078293@qq.com>
2019-04-28 13:05:20 +0800

    feat:implement Employee

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,60 @@
 package com.example.employee.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private String gender;
+    private int companyId;
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id,String name, int age, String gender, int companyId, int salary) {
+        this.id=id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 
 }
