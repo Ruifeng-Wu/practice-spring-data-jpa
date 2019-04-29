8024cb486d542f3f3734ed52a86b48bebabe10c5
Ruifeng-Wu <296078293@qq.com>
2019-04-29 12:12:36 +0800

    feat:implement practice 6

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import com.example.employee.entity.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -25,9 +26,11 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Page<Employee> findAll(Pageable pageable);
 
     //5.查找**的所在的公司的公司名称
-    @Query(value = "select c.companyName from Employee e left join Company c on c.id = e.companyId where e.name = ?1", nativeQuery = true)
+    @Query(value = "SELECT c.companyName FROM Employee e LEFT JOIN Company c ON c.id = e.companyId WHERE e.name = ?1", nativeQuery = true)
     String findByNameGetCompanyName(String name);
     //6.将*的名字改成*,输出这次修改影响的行数
-
+    @Modifying
+    @Query(value = "UPDATE Employee SET name = ?1 WHERE name = ?2", nativeQuery = true)
+    Integer updateName(String newName, String oldName);
     //7.删除姓名是*的employee
 }
